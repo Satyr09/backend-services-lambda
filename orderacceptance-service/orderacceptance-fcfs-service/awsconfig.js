cb6cb1c0169463fd9a5680d88ede9ecec3e61125
Daipayan <daisatyr09@gmail.com>
2021-01-18 10:51:22 +0530

    Add fifo queue endpoint lambda - fcfs service

---
+++ orderacceptance-service/orderacceptance-fcfs-service/awsconfig.js
@@ -0,0 +1,5 @@
+const awsConfig = {
+    ORDER_ACCEPTANCE_DB : "ServiceOrder"
+}
+
+module.exports = awsConfig;
---
+++ orderacceptance-service/orderacceptance-fcfs-service/index.js
@@ -0,0 +1,30 @@
+const AWS = require("aws-sdk")
+const DynamoDB = new AWS.DynamoDB();
+const awsConfig = require("./awsconfig");
+
+const ORDER_ACCEPTANCE_DB = awsConfig.ORDER_ACCEPTANCE_DB;
+exports.handler = async (event, context) => {
+    const record = event.Records[0];
+    const { body } = record;
+    const data = JSON.parse(body);
+    
+    console.log(data);
+    console.log("service provider id : ", data.serviceProviderId);
+    const dynamodbParams = {
+        TableName: ORDER_ACCEPTANCE_DB,
+        Item: {
+            "ServiceOrderId": {S : data.customerOrderId},
+            "serviceProviderId": {S : data.serviceProviderId},
+            "createdAt": {S : new Date().getTime().toString()}
+        },
+        ConditionExpression: 'attribute_not_exists(ServiceOrderId)'
+    };
+    try{
+        const info = await DynamoDB.putItem(dynamodbParams).promise();
+        console.log("DID NOT ERROR OUT")
+        console.log(info, "INFORMATION POSTED");
+        context.succeed(body);
+    }catch(err) {
+        context.fail(err);
+    }
+}
